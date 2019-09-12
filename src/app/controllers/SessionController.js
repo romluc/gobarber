eebc2136672dea3a58047a32e42ef4080bd67bcd
romluc <romulofilhofilho@gmail.com>
2019-09-12 13:35:32 -0300

    Update Session and User controllers with avatar

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 
-import authConfig from '../../config/auth';
 import User from '../models/User';
+import File from '../models/File';
+import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
@@ -19,7 +20,16 @@ class SessionController {
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          atribbutes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(400).json({ error: 'User not found!' });
@@ -29,13 +39,14 @@ class SessionController {
       return res.status(400).json({ error: 'Password does not match!' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        avatar,
       },
       // MD5 generated
       token: jwt.sign({ id }, authConfig.secret, {
